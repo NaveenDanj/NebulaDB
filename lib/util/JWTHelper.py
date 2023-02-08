b29dfe151eab459fc93cdb3108a7a434976ca0b6
Naveen Hettiwaththa <naveenhettiwaththa@gmail.com>
2023-02-08 19:35:19 +0530

    added jwt helper functions

---
+++ lib/util/JWTHelper.py
@@ -0,0 +1,37 @@
+import datetime
+import jwt
+import os
+
+
+def encode_auth_token(payload):
+    """
+    Generates the Auth Token
+    :return: string
+    """
+    try:
+        payload = {
+            'exp':
+            datetime.datetime.utcnow() + datetime.timedelta(days=0, seconds=5),
+            'iat':
+            datetime.datetime.utcnow(),
+            'sub':
+            payload
+        }
+        return jwt.encode(payload, os.environ.get("SECRET"), algorithm='HS256')
+    except Exception as e:
+        return e
+
+
+def decode_auth_token(auth_token):
+    """
+    Decodes the auth token
+    :param auth_token:
+    :return: integer|string
+    """
+    try:
+        payload = jwt.decode(auth_token, os.environ.get("SECRET"))
+        return payload['sub']
+    except jwt.ExpiredSignatureError:
+        return 'Signature expired. Please log in again.'
+    except jwt.InvalidTokenError:
+        return 'Invalid token. Please log in again.'
